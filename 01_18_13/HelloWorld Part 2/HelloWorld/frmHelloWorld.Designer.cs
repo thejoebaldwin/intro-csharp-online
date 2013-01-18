b73d9ca35497ac305c08a3afdb0f0b12c90294ce
thejoebaldwin <baldwinjoe@gmail.com>
2013-01-18 16:28:51 -0600

    Lecture Part II
    http://www.youtube.com/watch?v=MHVXyPmMKpY

---
+++ 01_18_13/HelloWorld Part 2/HelloWorld/frmHelloWorld.Designer.cs
@@ -0,0 +1,102 @@
+﻿namespace HelloWorld
+{
+    partial class frmHelloWorld
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblHelloWorld = new System.Windows.Forms.Label();
+            this.lblGreeting = new System.Windows.Forms.Label();
+            this.lblBlank = new System.Windows.Forms.Label();
+            this.btnGoodbye = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // lblHelloWorld
+            // 
+            this.lblHelloWorld.AutoSize = true;
+            this.lblHelloWorld.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblHelloWorld.Location = new System.Drawing.Point(157, 61);
+            this.lblHelloWorld.Name = "lblHelloWorld";
+            this.lblHelloWorld.Size = new System.Drawing.Size(120, 24);
+            this.lblHelloWorld.TabIndex = 0;
+            this.lblHelloWorld.Text = "Hello World";
+            // 
+            // lblGreeting
+            // 
+            this.lblGreeting.AutoSize = true;
+            this.lblGreeting.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblGreeting.Location = new System.Drawing.Point(176, 131);
+            this.lblGreeting.Name = "lblGreeting";
+            this.lblGreeting.Size = new System.Drawing.Size(0, 24);
+            this.lblGreeting.TabIndex = 1;
+            // 
+            // lblBlank
+            // 
+            this.lblBlank.AutoSize = true;
+            this.lblBlank.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblBlank.Location = new System.Drawing.Point(176, 214);
+            this.lblBlank.Name = "lblBlank";
+            this.lblBlank.Size = new System.Drawing.Size(83, 24);
+            this.lblBlank.TabIndex = 2;
+            this.lblBlank.Text = "lblBlank";
+            // 
+            // btnGoodbye
+            // 
+            this.btnGoodbye.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnGoodbye.Location = new System.Drawing.Point(140, 263);
+            this.btnGoodbye.Name = "btnGoodbye";
+            this.btnGoodbye.Size = new System.Drawing.Size(160, 49);
+            this.btnGoodbye.TabIndex = 3;
+            this.btnGoodbye.Text = "Goodbye!";
+            this.btnGoodbye.UseVisualStyleBackColor = true;
+            this.btnGoodbye.Click += new System.EventHandler(this.btnGoodbye_Click);
+            // 
+            // frmHelloWorld
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(506, 335);
+            this.Controls.Add(this.btnGoodbye);
+            this.Controls.Add(this.lblBlank);
+            this.Controls.Add(this.lblGreeting);
+            this.Controls.Add(this.lblHelloWorld);
+            this.Name = "frmHelloWorld";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.frmHelloWorld_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblHelloWorld;
+        private System.Windows.Forms.Label lblGreeting;
+        private System.Windows.Forms.Label lblBlank;
+        private System.Windows.Forms.Button btnGoodbye;
+    }
+}
+
---
+++ 01_18_13/HelloWorld Part 2/HelloWorld/frmHelloWorld.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace HelloWorld
+{
+    public partial class frmHelloWorld : Form
+    {
+        public frmHelloWorld()
+        {
+            InitializeComponent();
+        }
+
+        private void frmHelloWorld_Load(object sender, EventArgs e)
+        {
+            lblBlank.Text = "";
+        }
+
+        private void btnGoodbye_Click(object sender, EventArgs e)
+        {
+            lblGreeting.Text = "Goodbye!";
+        }
+    }
+}
