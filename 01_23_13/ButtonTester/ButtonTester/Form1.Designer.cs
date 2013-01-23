86780ce74f7fe660c5563455c3bb94a3c5f25755
thejoebaldwin <baldwinjoe@gmail.com>
2013-01-23 10:50:35 -0600

    Code from 1/23 lecture
    lecture at http://www.youtube.com/watch?v=i7CUCSp2czc

---
+++ 01_23_13/ButtonTester/ButtonTester/Form1.Designer.cs
@@ -0,0 +1,118 @@
+﻿namespace ButtonTester
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblStartup = new System.Windows.Forms.Label();
+            this.btnName = new System.Windows.Forms.Button();
+            this.lblButtonClick = new System.Windows.Forms.Label();
+            this.btnClear = new System.Windows.Forms.Button();
+            this.btnExit = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // lblStartup
+            // 
+            this.lblStartup.AutoSize = true;
+            this.lblStartup.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblStartup.Location = new System.Drawing.Point(128, 54);
+            this.lblStartup.Name = "lblStartup";
+            this.lblStartup.Size = new System.Drawing.Size(109, 24);
+            this.lblStartup.TabIndex = 0;
+            this.lblStartup.Text = "<lblStartup>";
+            // 
+            // btnName
+            // 
+            this.btnName.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnName.Location = new System.Drawing.Point(23, 195);
+            this.btnName.Name = "btnName";
+            this.btnName.Size = new System.Drawing.Size(105, 41);
+            this.btnName.TabIndex = 1;
+            this.btnName.Text = "Click Me!";
+            this.btnName.UseVisualStyleBackColor = true;
+            this.btnName.Click += new System.EventHandler(this.btnName_Click);
+            // 
+            // lblButtonClick
+            // 
+            this.lblButtonClick.AutoSize = true;
+            this.lblButtonClick.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblButtonClick.Location = new System.Drawing.Point(128, 97);
+            this.lblButtonClick.Name = "lblButtonClick";
+            this.lblButtonClick.Size = new System.Drawing.Size(144, 24);
+            this.lblButtonClick.TabIndex = 2;
+            this.lblButtonClick.Text = "<lblButtonClick>";
+            // 
+            // btnClear
+            // 
+            this.btnClear.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnClear.Location = new System.Drawing.Point(160, 195);
+            this.btnClear.Name = "btnClear";
+            this.btnClear.Size = new System.Drawing.Size(75, 41);
+            this.btnClear.TabIndex = 3;
+            this.btnClear.Text = "Clear";
+            this.btnClear.UseVisualStyleBackColor = true;
+            this.btnClear.Click += new System.EventHandler(this.btnClear_Click);
+            // 
+            // btnExit
+            // 
+            this.btnExit.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnExit.Location = new System.Drawing.Point(279, 195);
+            this.btnExit.Name = "btnExit";
+            this.btnExit.Size = new System.Drawing.Size(75, 41);
+            this.btnExit.TabIndex = 4;
+            this.btnExit.Text = "Exit";
+            this.btnExit.UseVisualStyleBackColor = true;
+            this.btnExit.Click += new System.EventHandler(this.btnExit_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(423, 262);
+            this.Controls.Add(this.btnExit);
+            this.Controls.Add(this.btnClear);
+            this.Controls.Add(this.lblButtonClick);
+            this.Controls.Add(this.btnName);
+            this.Controls.Add(this.lblStartup);
+            this.Name = "Form1";
+            this.Text = "Button Tester";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblStartup;
+        private System.Windows.Forms.Button btnName;
+        private System.Windows.Forms.Label lblButtonClick;
+        private System.Windows.Forms.Button btnClear;
+        private System.Windows.Forms.Button btnExit;
+    }
+}
+
---
+++ 01_23_13/ButtonTester/ButtonTester/Form1.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace ButtonTester
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            //sets the startup label on form load
+            lblStartup.Text = "This text is set on startup";
+            lblButtonClick.Text = "";
+        }
+
+        private void btnName_Click(object sender, EventArgs e)
+        {
+            //Setting the button label text on a button click
+            lblButtonClick.Text = "This text is set up button click";
+        }
+
+        private void btnClear_Click(object sender, EventArgs e)
+        {
+            //Clearing out both labels
+            lblButtonClick.Text = "";
+            lblStartup.Text = "";
+        }
+
+        private void btnExit_Click(object sender, EventArgs e)
+        {
+            //Closes the application
+            Application.Exit();
+        }
+    }
+}
