7d78f133dc828f8604a83184a2df3b8be39aa2e7
thejoebaldwin <baldwinjoe@gmail.com>
2013-02-02 16:05:38 -0600

    Review Application
    Code from lecture posted at http://youtu.be/VmZKnvSMRd4

---
+++ 02_02_13/ReviewApplication/ReviewApplication/frmReview.Designer.cs
@@ -0,0 +1,148 @@
+﻿namespace ReviewApplication
+{
+    partial class frmReview
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblGreeting = new System.Windows.Forms.Label();
+            this.lblResponse = new System.Windows.Forms.Label();
+            this.lblGoodbye = new System.Windows.Forms.Label();
+            this.btnResponse = new System.Windows.Forms.Button();
+            this.btnGoodbye = new System.Windows.Forms.Button();
+            this.btnClear = new System.Windows.Forms.Button();
+            this.btnExit = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // lblGreeting
+            // 
+            this.lblGreeting.AutoSize = true;
+            this.lblGreeting.Font = new System.Drawing.Font("MS PGothic", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblGreeting.ForeColor = System.Drawing.Color.Maroon;
+            this.lblGreeting.Location = new System.Drawing.Point(88, 26);
+            this.lblGreeting.Name = "lblGreeting";
+            this.lblGreeting.Size = new System.Drawing.Size(116, 19);
+            this.lblGreeting.TabIndex = 0;
+            this.lblGreeting.Text = "<lblGreeting>";
+            // 
+            // lblResponse
+            // 
+            this.lblResponse.AutoSize = true;
+            this.lblResponse.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblResponse.ForeColor = System.Drawing.Color.DarkBlue;
+            this.lblResponse.Location = new System.Drawing.Point(88, 66);
+            this.lblResponse.Name = "lblResponse";
+            this.lblResponse.Size = new System.Drawing.Size(158, 26);
+            this.lblResponse.TabIndex = 1;
+            this.lblResponse.Text = "<lblResponse>";
+            // 
+            // lblGoodbye
+            // 
+            this.lblGoodbye.AutoSize = true;
+            this.lblGoodbye.Font = new System.Drawing.Font("Kootenay", 8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblGoodbye.Location = new System.Drawing.Point(88, 99);
+            this.lblGoodbye.Name = "lblGoodbye";
+            this.lblGoodbye.Size = new System.Drawing.Size(75, 14);
+            this.lblGoodbye.TabIndex = 2;
+            this.lblGoodbye.Text = "<lblGoodbye>";
+            // 
+            // btnResponse
+            // 
+            this.btnResponse.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnResponse.Location = new System.Drawing.Point(12, 166);
+            this.btnResponse.Name = "btnResponse";
+            this.btnResponse.Size = new System.Drawing.Size(120, 35);
+            this.btnResponse.TabIndex = 3;
+            this.btnResponse.Text = "Response";
+            this.btnResponse.UseVisualStyleBackColor = true;
+            this.btnResponse.Click += new System.EventHandler(this.btnResponse_Click);
+            // 
+            // btnGoodbye
+            // 
+            this.btnGoodbye.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnGoodbye.Location = new System.Drawing.Point(138, 166);
+            this.btnGoodbye.Name = "btnGoodbye";
+            this.btnGoodbye.Size = new System.Drawing.Size(120, 35);
+            this.btnGoodbye.TabIndex = 4;
+            this.btnGoodbye.Text = "Goodbye";
+            this.btnGoodbye.UseVisualStyleBackColor = true;
+            this.btnGoodbye.Click += new System.EventHandler(this.btnGoodbye_Click);
+            // 
+            // btnClear
+            // 
+            this.btnClear.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnClear.Location = new System.Drawing.Point(264, 166);
+            this.btnClear.Name = "btnClear";
+            this.btnClear.Size = new System.Drawing.Size(120, 35);
+            this.btnClear.TabIndex = 5;
+            this.btnClear.Text = "Clear";
+            this.btnClear.UseVisualStyleBackColor = true;
+            this.btnClear.Click += new System.EventHandler(this.btnClear_Click);
+            // 
+            // btnExit
+            // 
+            this.btnExit.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnExit.Location = new System.Drawing.Point(97, 215);
+            this.btnExit.Name = "btnExit";
+            this.btnExit.Size = new System.Drawing.Size(120, 35);
+            this.btnExit.TabIndex = 6;
+            this.btnExit.Text = "Exit";
+            this.btnExit.UseVisualStyleBackColor = true;
+            this.btnExit.Click += new System.EventHandler(this.btnExit_Click);
+            // 
+            // frmReview
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Orange;
+            this.ClientSize = new System.Drawing.Size(414, 262);
+            this.Controls.Add(this.btnExit);
+            this.Controls.Add(this.btnClear);
+            this.Controls.Add(this.btnGoodbye);
+            this.Controls.Add(this.btnResponse);
+            this.Controls.Add(this.lblGoodbye);
+            this.Controls.Add(this.lblResponse);
+            this.Controls.Add(this.lblGreeting);
+            this.Name = "frmReview";
+            this.Text = "Review Application";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblGreeting;
+        private System.Windows.Forms.Label lblResponse;
+        private System.Windows.Forms.Label lblGoodbye;
+        private System.Windows.Forms.Button btnResponse;
+        private System.Windows.Forms.Button btnGoodbye;
+        private System.Windows.Forms.Button btnClear;
+        private System.Windows.Forms.Button btnExit;
+    }
+}
+
---
+++ 02_02_13/ReviewApplication/ReviewApplication/frmReview.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace ReviewApplication
+{
+    public partial class frmReview : Form
+    {
+        public frmReview()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            //Set the greeting label to hello there
+            //  clear out the other labels
+            lblGreeting.Text = "Hello There";
+            lblResponse.Text = "";
+            lblGoodbye.Text = "";
+        }
+
+        private void btnResponse_Click(object sender, EventArgs e)
+        {
+            //set the response label to hi, how are you
+            lblResponse.Text = "Hi, how are you?";
+        }
+
+        private void btnGoodbye_Click(object sender, EventArgs e)
+        {
+            //set the good bye label to fine thanks, goodbye
+            lblGoodbye.Text = "Fine thanks, Goodbye";
+        }
+
+        private void btnClear_Click(object sender, EventArgs e)
+        {
+            //clear out all the labels
+            lblGoodbye.Text = "";
+            lblGreeting.Text = "";
+            lblResponse.Text = "";
+        }
+
+        private void btnExit_Click(object sender, EventArgs e)
+        {
+            //close the application
+            Application.Exit();
+        }
+    }
+}
