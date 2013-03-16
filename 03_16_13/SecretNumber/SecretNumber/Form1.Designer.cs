c2f34b441de7a9630f8715ce69530028ecfb972a
thejoebaldwin <baldwinjoe@gmail.com>
2013-03-16 15:09:30 -0500

    If Statements

---
+++ 03_16_13/SecretNumber/SecretNumber/Form1.Designer.cs
@@ -0,0 +1,121 @@
+﻿namespace SecretNumber
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtGuess = new System.Windows.Forms.TextBox();
+            this.btnGuess = new System.Windows.Forms.Button();
+            this.btnQuit = new System.Windows.Forms.Button();
+            this.lblMessage = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // txtGuess
+            // 
+            this.txtGuess.Font = new System.Drawing.Font("Microsoft Sans Serif", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtGuess.Location = new System.Drawing.Point(119, 52);
+            this.txtGuess.Name = "txtGuess";
+            this.txtGuess.Size = new System.Drawing.Size(153, 44);
+            this.txtGuess.TabIndex = 0;
+            this.txtGuess.Text = "67";
+            this.txtGuess.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.txtGuess.TextChanged += new System.EventHandler(this.txtGuess_TextChanged);
+            // 
+            // btnGuess
+            // 
+            this.btnGuess.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnGuess.Location = new System.Drawing.Point(56, 174);
+            this.btnGuess.Name = "btnGuess";
+            this.btnGuess.Size = new System.Drawing.Size(121, 39);
+            this.btnGuess.TabIndex = 3;
+            this.btnGuess.Text = "&Guess";
+            this.btnGuess.UseVisualStyleBackColor = true;
+            this.btnGuess.Click += new System.EventHandler(this.btnGuess_Click);
+            // 
+            // btnQuit
+            // 
+            this.btnQuit.DialogResult = System.Windows.Forms.DialogResult.Cancel;
+            this.btnQuit.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnQuit.Location = new System.Drawing.Point(221, 174);
+            this.btnQuit.Name = "btnQuit";
+            this.btnQuit.Size = new System.Drawing.Size(121, 39);
+            this.btnQuit.TabIndex = 2;
+            this.btnQuit.Text = "&Quit";
+            this.btnQuit.UseVisualStyleBackColor = true;
+            this.btnQuit.Click += new System.EventHandler(this.btnQuit_Click);
+            // 
+            // lblMessage
+            // 
+            this.lblMessage.AutoSize = true;
+            this.lblMessage.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblMessage.Location = new System.Drawing.Point(35, 121);
+            this.lblMessage.Name = "lblMessage";
+            this.lblMessage.Size = new System.Drawing.Size(128, 24);
+            this.lblMessage.TabIndex = 3;
+            this.lblMessage.Text = "<lblMessage>";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(83, 9);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(273, 24);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Please enter the secret number";
+            // 
+            // Form1
+            // 
+            this.AcceptButton = this.btnGuess;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.CancelButton = this.btnQuit;
+            this.ClientSize = new System.Drawing.Size(422, 262);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.lblMessage);
+            this.Controls.Add(this.btnQuit);
+            this.Controls.Add(this.btnGuess);
+            this.Controls.Add(this.txtGuess);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtGuess;
+        private System.Windows.Forms.Button btnGuess;
+        private System.Windows.Forms.Button btnQuit;
+        private System.Windows.Forms.Label lblMessage;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ 03_16_13/SecretNumber/SecretNumber/Form1.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace SecretNumber
+{
+    public partial class Form1 : Form
+    {
+        const int SECRET_NUMBER = 67;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnQuit_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void btnGuess_Click(object sender, EventArgs e)
+        {
+            //declare variables
+            int intGuess = 0;
+            //get the user input
+            Int32.TryParse(txtGuess.Text, out intGuess);
+         
+            //output the result to the user
+
+            //not needed because clear on text changed
+            //lblMessage.Text = string.Empty;
+
+            //= assignment operator
+            //+,-,/,*,% arithmetic operators
+            //Comparison operators:
+            // == equality operator
+            // != inequality operator (<> in visual basic)
+            // <, <= less than operator , less than or equal to
+            // >, >= greater than operator, greater than or equal to
+            // ( 5 < 6) true
+            // (5 > 6) false
+            // (5 <= 6) true
+            // (5 >= 6) false
+
+            //do logic
+            //  Test if input equals our secret number (SECRET_NUMBER)
+            if (intGuess == SECRET_NUMBER)
+            {
+                //execute this code here if true
+                lblMessage.Text = "You Guessed the correct number!";
+            }
+            else
+            {
+                //execute this code here if false
+                lblMessage.Text = "You did not guess the correct number!";
+            }
+
+            // 5 != 6 This is True
+            //this is replace by the above else statement
+            //if (intGuess != SECRET_NUMBER)
+            //{
+             
+            //}
+
+            txtGuess.Focus();
+            txtGuess.SelectAll();
+        }
+
+        private void txtGuess_TextChanged(object sender, EventArgs e)
+        {
+            lblMessage.Text = string.Empty;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            lblMessage.Text = string.Empty;
+        }
+    }
+}
