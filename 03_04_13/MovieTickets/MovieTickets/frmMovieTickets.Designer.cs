9220abd3c1c8dd87643e91f2e4e11cb3ed640d9a
thejoebaldwin <baldwinjoe@gmail.com>
2013-03-19 11:28:39 -0500

    Learning Plan 2 Review

---
+++ 03_04_13/MovieTickets/MovieTickets/frmMovieTickets.Designer.cs
@@ -0,0 +1,232 @@
+﻿namespace MovieTickets
+{
+    partial class frmMovieTickets
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnSubmit = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtAdultQuantity = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.txtKidQuantity = new System.Windows.Forms.TextBox();
+            this.lblAdultCost = new System.Windows.Forms.Label();
+            this.lblKidCost = new System.Windows.Forms.Label();
+            this.btnClear = new System.Windows.Forms.Button();
+            this.btnExit = new System.Windows.Forms.Button();
+            this.lblTotal = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btnSubmit
+            // 
+            this.btnSubmit.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnSubmit.Location = new System.Drawing.Point(55, 274);
+            this.btnSubmit.Name = "btnSubmit";
+            this.btnSubmit.Size = new System.Drawing.Size(111, 38);
+            this.btnSubmit.TabIndex = 2;
+            this.btnSubmit.Text = "&Submit";
+            this.btnSubmit.UseVisualStyleBackColor = true;
+            this.btnSubmit.Click += new System.EventHandler(this.btnSubmit_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(163, 215);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(56, 24);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Total:";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.TopRight;
+            // 
+            // txtAdultQuantity
+            // 
+            this.txtAdultQuantity.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtAdultQuantity.Location = new System.Drawing.Point(251, 113);
+            this.txtAdultQuantity.Name = "txtAdultQuantity";
+            this.txtAdultQuantity.Size = new System.Drawing.Size(100, 29);
+            this.txtAdultQuantity.TabIndex = 0;
+            this.txtAdultQuantity.Text = "2";
+            this.txtAdultQuantity.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
+            this.txtAdultQuantity.TextChanged += new System.EventHandler(this.txtAdultQuantity_TextChanged);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(152, 24);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(67, 24);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Adults:";
+            this.label2.TextAlign = System.Drawing.ContentAlignment.TopRight;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(168, 58);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(51, 24);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Kids:";
+            this.label3.TextAlign = System.Drawing.ContentAlignment.TopRight;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(117, 118);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(102, 24);
+            this.label4.TabIndex = 5;
+            this.label4.Text = "# of Adults:";
+            this.label4.TextAlign = System.Drawing.ContentAlignment.TopRight;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(133, 156);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(86, 24);
+            this.label5.TabIndex = 6;
+            this.label5.Text = "# of Kids:";
+            this.label5.TextAlign = System.Drawing.ContentAlignment.TopRight;
+            // 
+            // txtKidQuantity
+            // 
+            this.txtKidQuantity.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtKidQuantity.Location = new System.Drawing.Point(251, 161);
+            this.txtKidQuantity.Name = "txtKidQuantity";
+            this.txtKidQuantity.Size = new System.Drawing.Size(100, 29);
+            this.txtKidQuantity.TabIndex = 1;
+            this.txtKidQuantity.Text = "1";
+            this.txtKidQuantity.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
+            this.txtKidQuantity.TextChanged += new System.EventHandler(this.txtKidQuantity_TextChanged);
+            // 
+            // lblAdultCost
+            // 
+            this.lblAdultCost.AutoSize = true;
+            this.lblAdultCost.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblAdultCost.Location = new System.Drawing.Point(225, 24);
+            this.lblAdultCost.Name = "lblAdultCost";
+            this.lblAdultCost.Size = new System.Drawing.Size(131, 24);
+            this.lblAdultCost.TabIndex = 8;
+            this.lblAdultCost.Text = "<lblAdultCost>";
+            // 
+            // lblKidCost
+            // 
+            this.lblKidCost.AutoSize = true;
+            this.lblKidCost.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblKidCost.Location = new System.Drawing.Point(225, 58);
+            this.lblKidCost.Name = "lblKidCost";
+            this.lblKidCost.Size = new System.Drawing.Size(115, 24);
+            this.lblKidCost.TabIndex = 9;
+            this.lblKidCost.Text = "<lblKidCost>";
+            // 
+            // btnClear
+            // 
+            this.btnClear.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnClear.Location = new System.Drawing.Point(229, 274);
+            this.btnClear.Name = "btnClear";
+            this.btnClear.Size = new System.Drawing.Size(111, 38);
+            this.btnClear.TabIndex = 3;
+            this.btnClear.Text = "&Clear";
+            this.btnClear.UseVisualStyleBackColor = true;
+            this.btnClear.Click += new System.EventHandler(this.btnClear_Click);
+            // 
+            // btnExit
+            // 
+            this.btnExit.DialogResult = System.Windows.Forms.DialogResult.Cancel;
+            this.btnExit.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnExit.Location = new System.Drawing.Point(397, 274);
+            this.btnExit.Name = "btnExit";
+            this.btnExit.Size = new System.Drawing.Size(111, 38);
+            this.btnExit.TabIndex = 4;
+            this.btnExit.Text = "&Exit";
+            this.btnExit.UseVisualStyleBackColor = true;
+            this.btnExit.Click += new System.EventHandler(this.btnExit_Click);
+            // 
+            // lblTotal
+            // 
+            this.lblTotal.AutoSize = true;
+            this.lblTotal.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblTotal.Location = new System.Drawing.Point(248, 215);
+            this.lblTotal.Name = "lblTotal";
+            this.lblTotal.Size = new System.Drawing.Size(92, 24);
+            this.lblTotal.TabIndex = 12;
+            this.lblTotal.Text = "<lblTotal>";
+            // 
+            // frmMovieTickets
+            // 
+            this.AcceptButton = this.btnSubmit;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.CancelButton = this.btnExit;
+            this.ClientSize = new System.Drawing.Size(551, 343);
+            this.Controls.Add(this.lblTotal);
+            this.Controls.Add(this.btnExit);
+            this.Controls.Add(this.btnClear);
+            this.Controls.Add(this.lblKidCost);
+            this.Controls.Add(this.lblAdultCost);
+            this.Controls.Add(this.txtKidQuantity);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txtAdultQuantity);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnSubmit);
+            this.Name = "frmMovieTickets";
+            this.Text = "Movie Tickets";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnSubmit;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtAdultQuantity;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox txtKidQuantity;
+        private System.Windows.Forms.Label lblAdultCost;
+        private System.Windows.Forms.Label lblKidCost;
+        private System.Windows.Forms.Button btnClear;
+        private System.Windows.Forms.Button btnExit;
+        private System.Windows.Forms.Label lblTotal;
+    }
+}
+
---
+++ 03_04_13/MovieTickets/MovieTickets/frmMovieTickets.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace MovieTickets
+{
+    public partial class frmMovieTickets : Form
+    {
+        //Constant values used for calculations
+        const double ADULT_COST = 15;
+        const double KID_COST = 5;
+
+
+        public frmMovieTickets()
+        {
+            InitializeComponent();
+        }
+
+        private void btnExit_Click(object sender, EventArgs e)
+        {
+            //closes application
+            Application.Exit();
+        }
+
+        private void btnClear_Click(object sender, EventArgs e)
+        {
+            //clear out all input and output
+            //   and return focus
+            txtAdultQuantity.Clear();
+            txtKidQuantity.Clear();
+            lblTotal.Text = String.Empty;
+            txtAdultQuantity.Focus();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            //clear out total placeholder text
+            lblTotal.Text = string.Empty;
+            //populate pricing display from constants
+            lblAdultCost.Text = ADULT_COST.ToString("c");
+            lblKidCost.Text = KID_COST.ToString("c");
+        }
+
+        private void btnSubmit_Click(object sender, EventArgs e)
+        {
+            //declare variables
+            Double dblAdultQuantity = 0;
+            Double dblKidQuantity = 0;
+            Double dblAdultCost = 0;
+            Double dblKidCost = 0;
+            Double dblTotal = 0;
+            //get user input
+            Double.TryParse(txtAdultQuantity.Text, out dblAdultQuantity);
+            Double.TryParse(txtKidQuantity.Text, out dblKidQuantity);
+            //do calculations
+            //  adult cost = # of adults x ADULT_COST
+            dblAdultCost = dblAdultQuantity * ADULT_COST;
+            //  kid cost = # of kids x KID_COST
+            dblKidCost = dblKidQuantity * KID_COST;
+            //  total = adult cost + kid cost
+            dblTotal = dblAdultCost + dblKidCost;
+            //output results to user
+            lblTotal.Text = dblTotal.ToString("c");
+            //return focus to first textbox
+            txtAdultQuantity.Focus();
+            txtAdultQuantity.SelectAll();
+
+        }
+
+        private void txtAdultQuantity_TextChanged(object sender, EventArgs e)
+        {
+            //clear out results on user entry
+            lblTotal.Text = string.Empty;
+        }
+
+        private void txtKidQuantity_TextChanged(object sender, EventArgs e)
+        {
+            //clear out results on user entry
+            lblTotal.Text = string.Empty;
+        }
+    }
+}
