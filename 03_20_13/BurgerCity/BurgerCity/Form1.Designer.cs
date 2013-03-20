4e34d0c3a927e949858ecd32be44eb9c9bd4a58e
thejoebaldwin <baldwinjoe@gmail.com>
2013-03-20 14:22:52 -0500

    Checkboxes and Functions

---
+++ 03_20_13/BurgerCity/BurgerCity/Form1.Designer.cs
@@ -0,0 +1,135 @@
+﻿namespace BurgerCity
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.chkLettuce = new System.Windows.Forms.CheckBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.lblTotal = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.chkCheese = new System.Windows.Forms.CheckBox();
+            this.chkOnions = new System.Windows.Forms.CheckBox();
+            this.SuspendLayout();
+            // 
+            // chkLettuce
+            // 
+            this.chkLettuce.AutoSize = true;
+            this.chkLettuce.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.chkLettuce.Location = new System.Drawing.Point(83, 70);
+            this.chkLettuce.Name = "chkLettuce";
+            this.chkLettuce.Size = new System.Drawing.Size(90, 28);
+            this.chkLettuce.TabIndex = 0;
+            this.chkLettuce.Text = "Lettuce";
+            this.chkLettuce.UseVisualStyleBackColor = true;
+            this.chkLettuce.CheckedChanged += new System.EventHandler(this.chkLettuce_CheckedChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(24, 43);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(240, 24);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Please select your toppings";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // lblTotal
+            // 
+            this.lblTotal.AutoSize = true;
+            this.lblTotal.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblTotal.Location = new System.Drawing.Point(33, 202);
+            this.lblTotal.Name = "lblTotal";
+            this.lblTotal.Size = new System.Drawing.Size(92, 24);
+            this.lblTotal.TabIndex = 2;
+            this.lblTotal.Text = "<lblTotal>";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(24, 9);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(195, 24);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "All hamburgers are $5";
+            // 
+            // chkCheese
+            // 
+            this.chkCheese.AutoSize = true;
+            this.chkCheese.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.chkCheese.Location = new System.Drawing.Point(83, 113);
+            this.chkCheese.Name = "chkCheese";
+            this.chkCheese.Size = new System.Drawing.Size(95, 28);
+            this.chkCheese.TabIndex = 5;
+            this.chkCheese.Text = "Cheese";
+            this.chkCheese.UseVisualStyleBackColor = true;
+            this.chkCheese.CheckedChanged += new System.EventHandler(this.chkCheese_CheckedChanged);
+            // 
+            // chkOnions
+            // 
+            this.chkOnions.AutoSize = true;
+            this.chkOnions.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.chkOnions.Location = new System.Drawing.Point(83, 151);
+            this.chkOnions.Name = "chkOnions";
+            this.chkOnions.Size = new System.Drawing.Size(90, 28);
+            this.chkOnions.TabIndex = 6;
+            this.chkOnions.Text = "Onions";
+            this.chkOnions.UseVisualStyleBackColor = true;
+            this.chkOnions.CheckedChanged += new System.EventHandler(this.chkOnions_CheckedChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(393, 314);
+            this.Controls.Add(this.chkOnions);
+            this.Controls.Add(this.chkCheese);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.lblTotal);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.chkLettuce);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.CheckBox chkLettuce;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label lblTotal;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.CheckBox chkCheese;
+        private System.Windows.Forms.CheckBox chkOnions;
+    }
+}
+
---
+++ 03_20_13/BurgerCity/BurgerCity/Form1.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace BurgerCity
+{
+    public partial class Form1 : Form
+    {
+        const double LETTUCE_COST = 1.0;
+        const double CHEESE_COST = 0.50;
+        const double ONION_COST = 0.25;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+
+
+
+
+
+        private void UpdatePrice()
+        {
+            //declare variables
+            double dblTotal = 5;
+            //bool blnLettuce = false;
+            //bool blnCheese = false;
+            //get user input
+            //blnLettuce = chkLettuce.Checked;
+            //blnCheese = chkCheese.Checked;
+            //do calculations
+            //  what toppings to add to total?
+            if (chkLettuce.Checked)
+            {
+                dblTotal = dblTotal + LETTUCE_COST;
+            }
+
+            if (chkCheese.Checked)
+            {
+                dblTotal = dblTotal + CHEESE_COST;
+            }
+
+            if (chkOnions.Checked)
+            {
+                dblTotal = dblTotal + ONION_COST;
+            }
+            //output result to user
+            lblTotal.Text = dblTotal.ToString("c");
+
+        }
+        
+        
+        
+     
+        private void chkLettuce_CheckedChanged(object sender, EventArgs e)
+        {
+            UpdatePrice();
+        }
+
+        private void chkCheese_CheckedChanged(object sender, EventArgs e)
+        {
+            UpdatePrice();
+        }
+
+        private void chkOnions_CheckedChanged(object sender, EventArgs e)
+        {
+            UpdatePrice();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            lblTotal.Text = string.Empty;
+        }
+    }
+}
