6610baec95fec89d4531540db945cd2b155c01e6
thejoebaldwin <baldwinjoe@gmail.com>
2013-05-02 13:53:56 -0500

    Introduction To Arrays

---
+++ 04_25_13/LoopWithArrays/LoopWithArrays/Form1.Designer.cs
@@ -0,0 +1,130 @@
+﻿namespace LoopWithArrays
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnLoad = new System.Windows.Forms.Button();
+            this.lbxStudents = new System.Windows.Forms.ListBox();
+            this.lblStudent = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.lblCredits = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btnLoad
+            // 
+            this.btnLoad.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnLoad.Location = new System.Drawing.Point(77, 218);
+            this.btnLoad.Name = "btnLoad";
+            this.btnLoad.Size = new System.Drawing.Size(97, 32);
+            this.btnLoad.TabIndex = 0;
+            this.btnLoad.Text = "Load";
+            this.btnLoad.UseVisualStyleBackColor = true;
+            this.btnLoad.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // lbxStudents
+            // 
+            this.lbxStudents.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbxStudents.FormattingEnabled = true;
+            this.lbxStudents.ItemHeight = 24;
+            this.lbxStudents.Location = new System.Drawing.Point(24, 12);
+            this.lbxStudents.Name = "lbxStudents";
+            this.lbxStudents.Size = new System.Drawing.Size(248, 196);
+            this.lbxStudents.TabIndex = 1;
+            this.lbxStudents.SelectedIndexChanged += new System.EventHandler(this.lbxStudents_SelectedIndexChanged);
+            // 
+            // lblStudent
+            // 
+            this.lblStudent.AutoSize = true;
+            this.lblStudent.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblStudent.Location = new System.Drawing.Point(374, 45);
+            this.lblStudent.Name = "lblStudent";
+            this.lblStudent.Size = new System.Drawing.Size(115, 24);
+            this.lblStudent.TabIndex = 2;
+            this.lblStudent.Text = "<lblStudent>";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(294, 45);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(79, 24);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Student:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(294, 134);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(73, 24);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Credits:";
+            // 
+            // lblCredits
+            // 
+            this.lblCredits.AutoSize = true;
+            this.lblCredits.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblCredits.Location = new System.Drawing.Point(374, 134);
+            this.lblCredits.Name = "lblCredits";
+            this.lblCredits.Size = new System.Drawing.Size(109, 24);
+            this.lblCredits.TabIndex = 5;
+            this.lblCredits.Text = "<lblCredits>";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(571, 262);
+            this.Controls.Add(this.lblCredits);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.lblStudent);
+            this.Controls.Add(this.lbxStudents);
+            this.Controls.Add(this.btnLoad);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnLoad;
+        private System.Windows.Forms.ListBox lbxStudents;
+        private System.Windows.Forms.Label lblStudent;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label lblCredits;
+
+    }
+}
+
---
+++ 04_25_13/LoopWithArrays/LoopWithArrays/Form1.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace LoopWithArrays
+{
+    public partial class Form1 : Form
+    {
+        string[] Students = {
+                                    "Suzy",
+                                    "Tom",
+                                    "Jeff",
+                                    "Betty",
+                                    "Paul",
+                                    "John"
+                                };
+        int[] Credits = {
+                            34,
+                            67,
+                            22,
+                            89,
+                            0,
+                            21
+
+
+                        };
+
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+           
+            int i = 0;
+            while (i < Students.Length)
+            {
+                lbxStudents.Items.Add(Students[i]);
+                i++;
+            }
+
+
+        }
+
+        private void lbxStudents_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            int i = lbxStudents.SelectedIndex;
+            lblStudent.Text = Students[i];
+            lblCredits.Text = Credits[i].ToString();
+
+        }
+    }
+}
